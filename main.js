ca6bd682691cb0e70a13d95a92fc427c437022b4
finntastic <179311210+finntaztic@users.noreply.github.com>
2026-01-24 11:24:19 +0800

    Add files

---
+++ main.js
@@ -0,0 +1,22 @@
+
+class Ship {
+    constructor(length, hit, sunk){
+        this.length = length;
+        this.hit = hit;
+        this.sunk = sunk;
+    }
+
+    hit (){
+        //function that increases the number of ‘hits’ in your ship.
+    }
+
+    isSunk(){
+        //calculates whether a ship is considered 
+        // sunk based on its length and the number of 
+        // hits it has received.
+    }
+}
+
+class Gameboard {
+
+}
