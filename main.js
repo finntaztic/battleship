8088b7acf9594a2d1cad2df51b1e811011a37d93
finntastic <179311210+finntaztic@users.noreply.github.com>
2026-01-25 20:05:46 +0800

    npm

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ class Ship {
         this.sunk = sunk;
     }
 
+    static checkTest (x){
+        return x + 1;
+    }
+
     hit (){
         //function that increases the number of ‘hits’ in your ship.
     }
@@ -19,4 +23,22 @@ class Ship {
 
 class Gameboard {
 
+    ship (){
+        //place ship to specific coordinates
+    }
+    receiveAttack (x, y){
+        //takes pair of coordinates, determine whether or not the attack hit a ship
+        //send the hit function to the correct ship
+        //record coordinates of the missed shot
+    }
+
+
+    //track missed attacks
+    //report whether or not all their ships have been sunked
 }
+
+class Player {
+
+}
+
+module.exports =  {Ship}
---
+++ test.js
@@ -0,0 +1,6 @@
+
+const {Ship} = require('./main')
+
+test('first test', () => {
+    expect (Ship.checkTest(1)).toBe(2)
+})
